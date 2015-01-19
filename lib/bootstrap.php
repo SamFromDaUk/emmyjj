5252cc30272d7569757790696c72d654c1e5cb69
James Wilson <jbw@me.com>
2015-01-19 10:37:30 +0000

    #JW Updated contact details

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -15,7 +15,7 @@ define('DB_USER', '');
 define('DB_PASS', '');
 
 define('FORM_FRESHNESS', 60*15);
-define('CONTACT_EMAIL', 'samwarren909@gmail.com');
-define('WEB_ADMIN_CONTACT', 'samwarren909@gmail.com');
+define('CONTACT_EMAIL', 'enquiries@emmyjj.com');
+define('WEB_ADMIN_CONTACT', 'enquiries@emmyjj.com');
 
 \EmmyJJ::setup();
